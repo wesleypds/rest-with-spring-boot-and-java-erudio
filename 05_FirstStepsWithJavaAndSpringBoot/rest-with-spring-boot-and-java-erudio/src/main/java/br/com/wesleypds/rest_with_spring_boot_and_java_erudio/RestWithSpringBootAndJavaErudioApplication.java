148bca6e526b427c240bf0f93cb573aa628936d7
wesleypds <wlypereiraa@gmail.com>
2025-02-11 18:33:25 -0300

    first commit

---
+++ 05_FirstStepsWithJavaAndSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/wesleypds/rest_with_spring_boot_and_java_erudio/RestWithSpringBootAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesleypds.rest_with_spring_boot_and_java_erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaErudioApplication.class, args);
+	}
+
+}
