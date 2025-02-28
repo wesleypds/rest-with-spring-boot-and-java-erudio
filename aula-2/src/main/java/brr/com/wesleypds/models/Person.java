785b8dfcd96096476e56c4bd58d535eee9110418
wesleypds <wlypereiraa@gmail.com>
2025-02-27 23:14:07 -0300

    :sparkles: feat: add comunicação com banco e mapeando entidade

--- aula-2/src/main/java/brr/com/wesleypds/models/Person.java
+++ aula-2/src/main/java/brr/com/wesleypds/models/Person.java
@@ -2,16 +2,31 @@ package brr.com.wesleypds.models;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Column(name = "address")
     private String address;
 
+    @Column(name = "gender")
     private String gender;
 
     public Person() {}
---
+++ aula-2/src/main/java/brr/com/wesleypds/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package brr.com.wesleypds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import brr.com.wesleypds.models.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
