5d71332e6c7925316ec827595b6abadea263af2f
wesleypds <wlypereiraa@gmail.com>
2025-03-13 18:02:39 -0300

    :sparkles: feat: configuração de Content Negotiation

---
+++ aula-4/src/main/java/brr/com/wesleypds/config/WebConfig.java
@@ -0,0 +1,30 @@
+package brr.com.wesleypds.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        // Via QUERY PARAM
+        // configurer.favorParameter(true)
+        // .parameterName("mediaType").ignoreAcceptHeader(true)
+        // .useRegisteredExtensionsOnly(false)
+        // .defaultContentType(MediaType.APPLICATION_JSON)
+        // .mediaType(".json", MediaType.APPLICATION_JSON)
+        // .mediaType(".xml", MediaType.APPLICATION_XML);
+
+        // VIA HEADER PARAM
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                    .mediaType(".json", MediaType.APPLICATION_JSON)
+                    .mediaType(".xml", MediaType.APPLICATION_XML);
+    }
+
+}
