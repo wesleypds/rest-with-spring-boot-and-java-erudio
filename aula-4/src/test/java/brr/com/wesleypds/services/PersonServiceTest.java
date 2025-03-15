f43cd6749d8a3e6a9b62578c72983036bd87b994
wesleypds <wlypereiraa@gmail.com>
2025-03-14 21:39:17 -0300

    :sparkles: feat: add testes unitários com mockito

---
+++ aula-4/src/test/java/brr/com/wesleypds/services/PersonServiceTest.java
@@ -0,0 +1,78 @@
+package brr.com.wesleypds.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import brr.com.wesleypds.models.Person;
+import brr.com.wesleypds.repositories.PersonRepository;
+import brr.com.wesleypds.unittests.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    MockPerson input;
+
+    @Mock
+    private PersonRepository repository;
+
+    @InjectMocks
+    private PersonService service;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+        var result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/people/v1/1>;rel=\"self\"]"));
+        assertEquals(person.getFirstName(), result.getFirstName());
+        assertEquals(person.getLastName(), result.getLastName());
+        assertEquals(person.getAddress(), result.getAddress());
+        assertEquals(person.getGender(), result.getGender());
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
