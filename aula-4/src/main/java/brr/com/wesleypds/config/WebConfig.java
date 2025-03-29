265c6b8133297930b29989d375d0940167910a2c
wesleypds <wlypereiraa@gmail.com>
2025-03-28 23:51:34 -0300

    :sparkles: feat: configurando CORS global para a aplicação

--- aula-4/src/main/java/brr/com/wesleypds/config/WebConfig.java
+++ aula-4/src/main/java/brr/com/wesleypds/config/WebConfig.java
@@ -2,10 +2,12 @@ package brr.com.wesleypds.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import brr.com.wesleypds.serialization.converter.YamlJackson2HttpMesageConverter;
@@ -14,6 +16,23 @@ import brr.com.wesleypds.serialization.converter.YamlJackson2HttpMesageConverter
 public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMesageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -30,14 +49,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
-                    .mediaType("json", MediaType.APPLICATION_JSON)
-                    .mediaType("xml", MediaType.APPLICATION_XML)
-                    .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
-    }
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new YamlJackson2HttpMesageConverter());
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
     }
 
 }
